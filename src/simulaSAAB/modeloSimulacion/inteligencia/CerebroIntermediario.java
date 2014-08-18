9bc9bec1ff4b0a30b182f7b6cca7020f7d29b6c8
luisagm85 <luisagm85@gmail.com>
2014-08-18 16:29:13 -0500

    Cerebro intermediario

--- src/simulaSAAB/modeloSimulacion/inteligencia/CerebroIntermediario.java
+++ src/simulaSAAB/modeloSimulacion/inteligencia/CerebroIntermediario.java
@@ -6,10 +6,17 @@ package simulaSAAB.modeloSimulacion.inteligencia;
 import java.util.ArrayList;
 import java.util.List;
 
+import repast.simphony.space.projection.Projection;
+import repast.simphony.util.ContextUtils;
+import repast.simphony.context.Context;
+import simulaSAAB.main.VariablesGlobales;
 import simulaSAAB.modeloSimulacion.Producto;
 import simulaSAAB.modeloSimulacion.agentes.Intermediario;
+import simulaSAAB.modeloSimulacion.comunicacion.Accion;
+import simulaSAAB.modeloSimulacion.comunicacion.Item;
 import simulaSAAB.modeloSimulacion.comunicacion.Proposito;
 import simulaSAAB.modeloSimulacion.tareas.SistemaActividadHumana;
+import simulaSAAB.persistencia.ConfiguracionStatelessEJB;
 
 /**
  * @author dampher
@@ -17,16 +24,43 @@ import simulaSAAB.modeloSimulacion.tareas.SistemaActividadHumana;
  */
 public class CerebroIntermediario implements CerebroDeDecision, CerebroDeAprendizaje{
 	
-	public List<Proposito> fijarPropositosIntermediacion(){/*
+	private Intermediario intermediario;
+	
+	public CerebroIntermediario(Intermediario i){
+		
+		this.intermediario = i;
+		
+	}
+	
+	@Override
+	public List<Proposito> fijarPropositos(List<Producto> productos){
 		
 		//Inicializa el proposito
 		ArrayList<Proposito> propositos = new ArrayList<Proposito>();
 		
-		Intermediario getLocation(){
+		//Lista productos en orden
+		productos = new ConfiguracionStatelessEJB("ASC").obtenerProductosConfigurados(productos);
+		
+		Object inter = this.intermediario;
+		
+		//Consulta contexto de intermediario
+		Context<Object> context = ContextUtils.getContext(inter);
+		
+		
+		for(Producto p: productos){
+		
+		if(context.getProjection(VariablesGlobales.RED_AGRORED)!= null){
 			
+			propositos.add(new Proposito(new Accion("Comprar"),new Item(p.getNombre(),"Producto"),"en la ubicación de oferta con menor valor"));
 		}
-		*/
-		return null;
+		else if (context.getProjection(VariablesGlobales.RED_NUTRIRED) != null){ 
+		
+				propositos.add(new Proposito(new Accion("Vender"),new Item(p.getNombre(),"Producto"),"en la ubicación de demanda con mayor valor"));
+			}
+		
+		}
+		
+		return propositos;
 	}
 
 	@Override
@@ -38,12 +72,6 @@ public class CerebroIntermediario implements CerebroDeDecision, CerebroDeAprendi
 	@Override
 	public List<SistemaActividadHumana> recordarExperiencia(
 			List<SistemaActividadHumana> act) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List<Proposito> fijarPropositos(List<Producto> productos) {
 		// TODO Auto-generated method stub
 		return null;
 	}
