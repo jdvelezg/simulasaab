7b4c5c10301e92a21276c28715e4680a71a149d0
jdvelezg <jdvelezg@users.noreply.github.com>
2014-08-21 17:33:01 -0500

    ajuste secuencia principal de acciones

--- src/simulaSAAB/modeloSimulacion/tareas/ProducirCebollaBulbo.java
+++ src/simulaSAAB/modeloSimulacion/tareas/ProducirCebollaBulbo.java
@@ -10,13 +10,18 @@ import simulaSAAB.persistencia.ProductoConfigurado;
 
 public abstract class ProducirCebollaBulbo implements SistemaActividadHumana {
 	
-	public Proposito proposito;
+	private Proposito proposito;
 	
-	public double CostoEjecucion;
+	private double CostoEjecucion;
 	
-	public Terreno terreno;
+	private Terreno terreno;
 	
-	public Producto cebollaBulbo;
+	private Producto cebollaBulbo;	
+	
+	private String Estado;
+	
+	private int paso;
+	
 	
 	/**
 	 * Constructor
@@ -25,11 +30,11 @@ public abstract class ProducirCebollaBulbo implements SistemaActividadHumana {
 	public ProducirCebollaBulbo(Terreno t){
 		
 		MPAConfigurado mpa 	=new MPAConfigurado("ProduccionCebollaBulbo");
-		Producto p 			=new Producto(new ProductoConfigurado("Cebolla de bulbo"));
 		
+		this.cebollaBulbo	=new Producto(new ProductoConfigurado("Cebolla de bulbo"));		
 		this.proposito 		=mpa.getProposito();
 		this.CostoEjecucion	=mpa.getCosto();
-		this.terreno		=t;
+		this.terreno		=t;	
 		
 	}
 
@@ -37,8 +42,59 @@ public abstract class ProducirCebollaBulbo implements SistemaActividadHumana {
 	public Proposito getProposito() {
 		return this.proposito;
 	}
-
+	
+	/**
+	 * 
+	 */
 	@Override
-	public abstract void secuenciaPrincipalDeAcciones(AgenteSaab actor);
+	public void secuenciaPrincipalDeAcciones(AgenteSaab actor){
+		if(this.Estado.compareToIgnoreCase(EstadoActividad.READY.toString())==1){
+			
+			this.Estado	=EstadoActividad.RUNNING.toString();
+			this.paso	=1;			
+		}
+		else{			
+			
+			switch(this.paso){
+			case 1:
+				
+				this.terreno.setEstado("Preparado");
+				this.terreno.setHaUsadas(this.terreno.getHectareas());
+				this.terreno.setUsoSuelo("Agricola");
+				actor.setEstado("Preparando");
+				
+				this.paso++;				
+				break;
+			case 2:
+				
+				this.terreno.setEstado("Sembrado");
+				actor.setEstado("Sembrando");
+				
+				this.paso++;				
+				break;
+			case 3:
+								
+				actor.setEstado("Cultivo");
+				
+				this.paso++;				
+				break;
+			case 4:				
+				
+				actor.setEstado("Control Cultivo");
+				
+				this.paso++;				
+				break;
+			case 5:
+				
+				
+				
+				this.paso++;				
+				break;
+			default:
+				this.Estado ="DONE";
+			}
+			
+		}	
+	}
 
 }
