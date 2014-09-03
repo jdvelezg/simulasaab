be95636a64dab71f0ef9020e9a0ad2a397c1e060
jdvelezg <jdvelezg@users.noreply.github.com>
2014-09-03 16:04:28 -0500

    COmpleting secuenciaPPalDeAcciones method

--- src/simulaSAAB/modeloSimulacion/tareas/VenderProductos.java
+++ src/simulaSAAB/modeloSimulacion/tareas/VenderProductos.java
@@ -4,21 +4,44 @@ import java.util.Iterator;
 import java.util.List;
 
 import repast.simphony.context.Context;
+import repast.simphony.engine.environment.RunEnvironment;
+import repast.simphony.space.graph.Network;
 import repast.simphony.util.ContextUtils;
 import repast.simphony.util.collections.IndexedIterable;
 import simulaSAAB.contextos.GeografiaFija;
+import simulaSAAB.main.VariablesGlobales;
 import simulaSAAB.modeloSimulacion.AmbienteLocal;
+import simulaSAAB.modeloSimulacion.comunicacion.Demanda;
+import simulaSAAB.modeloSimulacion.comunicacion.Oferta;
 import simulaSAAB.modeloSimulacion.CentroUrbano;
 import simulaSAAB.modeloSimulacion.agentes.AgenteSaab;
 import simulaSAAB.modeloSimulacion.comunicacion.Proposito;
 
 public abstract class VenderProductos implements SistemaActividadHumana {
+		
+	public String enunciado;
 	
-	String enunciado;
-	Proposito Proposito;
-	String Estado;
-	int paso;
+	public Proposito Proposito;
 	
+	public String Estado;
+	
+	public int paso;
+	
+	public final double Tickinicial;
+	
+	public Oferta Oferta;
+	
+	public CentroUrbano CentroUrbano;
+	
+	/**
+	 * Constructor
+	 * @param of
+	 */
+	public VenderProductos(Oferta of){
+		
+		this.Oferta	= of;
+		Tickinicial	= RunEnvironment.getInstance().getCurrentSchedule().getTickCount();
+	}
 
 	@Override
 	public String getEnunciado() {
@@ -47,108 +70,141 @@ public abstract class VenderProductos implements SistemaActividadHumana {
 
 	@Override
 	public void secuenciaPrincipalDeAcciones(AgenteSaab actor) {
-		// TODO Auto-generated method stub
-		
-		//obtiene ambiente local
-		AmbienteLocal amb=null;
-		
-		Context<Object> context 		= ContextUtils.getContext(actor);		
-		IndexedIterable<Object> index 	= context.getObjects(AmbienteLocal.class);
-		Iterator<Object> iter 			= index.iterator();
-		
-		while(iter.hasNext()){
-			
-			amb = (AmbienteLocal)iter.next();			
-			if(amb.getGeometria().intersects(actor.getGeometria().getGeometryN(0))){
-				break;
-			}
-		}
-		
-		//obtiene lista de centros urbanos		
-		if(amb!=null){
-			
-			List<CentroUrbano> centros = amb.getCentrosUrbanos();
-			double distance 	= -1;
-			double partialdist	= 0;
-			CentroUrbano target = null;
-			
-			//Devuelve centro urbano mas cercano
-			for(CentroUrbano u : centros){
-				
-				partialdist =u.getGeometria().distance(actor.getGeometria());
-				
-				if(distance > partialdist)
-					target = u;
-				
-			}
-		}
-				
-				
-				
-				//Obtiene la red de oferta y agrega la oferta ligada al centro urbano.
-				
-				//espera x numero de ticks
-				
-					// if sin respuesta - Obtiene red de demandas
-					
-					// Busca demanda de mayor precio **1
-					
-					//vende: EVIA MENSAJE "VENDO"
-					//sigue protocolo
-					
-					//mas producto? SI -> vuelve a **1
-					//NO ->FIN
-		
-				//recibe mensaje: "compro"
-				//ejecuta protocolo
-				//FIN
-		
-		
+		// TODO Auto-generated method stub	
 		if(this.Estado.compareToIgnoreCase(EstadoActividad.READY.toString())==1){
 			
 			this.Estado	=EstadoActividad.RUNNING.toString();
-			this.paso	=1;			
+			this.paso	=1;
+			
 		}
 		else if(this.Estado.compareToIgnoreCase(EstadoActividad.RUNNING.toString())==1){			
 			
 			switch(this.paso){
 			case 1:
 								
-				actor.setEstado("Preparando");
+				actor.setEstado("ofertando");
+				
+				//obtiene ambiente local
+				AmbienteLocal amb=null;
+				
+				Context<Object> context 		= ContextUtils.getContext(actor);		
+				IndexedIterable<Object> index 	= context.getObjects(AmbienteLocal.class);
+				Iterator<Object> iter 			= index.iterator();
+				
+				while(iter.hasNext()){
+					
+					amb = (AmbienteLocal)iter.next();			
+					if(amb.getGeometria().intersects(actor.getGeometria().getGeometryN(0))){
+						break;
+					}
+				}				
+						
+				//obtiene lista de centros urbanos		
+				if(amb!=null){
+					
+					List<CentroUrbano> centros = amb.getCentrosUrbanos();
+					double distance 	= -1;
+					double partialdist	= 0;					
+					
+					//Devuelve centro urbano mas cercano
+					for(CentroUrbano u : centros){				
+						partialdist =u.getGeometria().distance(actor.getGeometria());				
+						if(distance > partialdist)
+							CentroUrbano = u;				
+					}
+					
+					//Obtiene la red de oferta y agrega la oferta ligada al centro urbano.
+					if(CentroUrbano!=null){
+						Network OfertaNetwork = (Network)ContextUtils.getParentContext(context).getSubContext(VariablesGlobales.CONTEXTO_OFERTA).getProjection(VariablesGlobales.RED_OFERTA);
+						OfertaNetwork.addEdge(CentroUrbano, Oferta);
+					}else{
+						System.out.println("ERROR: IMPOSIBLE AGREGAR OFERTA; No se encontro centro urbano en el ambiente");
+						this.Estado ="READY";
+						break;
+					}
+					
+				}else{
+					//fallo
+					System.out.println("ERROR: IMPOSIBLE AGREGAR OFERTA; No se encontro un ambiente local para el agente");
+					this.Estado ="READY";
+					break;
+				}
 				
 				this.paso++;				
-				break;
+				break;				
 			case 2:
-				
 								
+				//espera x numero de ticks
 				
-				this.paso++;				
+				Double CurrentTick = RunEnvironment.getInstance().getCurrentSchedule().getTickCount();
+				
+				if(CurrentTick-Tickinicial>=10){
+					this.paso++;
+				}
+				
 				break;
 			case 3:
-								
-				
-				
-				this.paso++;				
+				if(Oferta.getEstado().compareToIgnoreCase("vendida")==0){
+					
+					this.paso	= 0;
+					this.Estado = "DONE";
+					actor.setEstado("IDLE");
+					break;
+					
+				}else{// if sin respuesta - busca en red las demandas
+					
+					this.paso++;
+				}
+												
 				break;
 			case 4:				
 				
+				// Busca demanda de mayor precio **1
+				Context<Object> OfferContext = ContextUtils.getContext(Oferta);
+				Network OfferNetwork 		 = (Network)OfferContext.getProjection(VariablesGlobales.RED_OFERTA);
+				Iterable Demandas			 = OfferNetwork.getEdges(CentroUrbano);
+				Iterator DemIter			 = Demandas.iterator();
+				Demanda demanda				 = null;
+				double tempPrecio			 =-1;
 				
+				while(DemIter.hasNext()){
+					
+					Demanda temp = (Demanda)DemIter.next();					
+					if(temp.getPrecio()>tempPrecio)
+						demanda = temp;
+					
+				}
+				//vende: ENVIA MENSAJE "VENDO"
+				demanda.getDemandante().sendMessage();
 				
 				this.paso++;				
 				break;
 			case 5:
 				
-							
+				//espera x numero de ticks
 				
-				this.paso++;				
+				Double CurrentTick2 = RunEnvironment.getInstance().getCurrentSchedule().getTickCount();
+				
+				if(CurrentTick2-Tickinicial>=10){
+					this.paso++;
+				}
+				
 				break;
 			case 6:
-				//ejecuta subsistema de actividad
-				
-				//verifica si el sistema termino
-				
-				//si termina suma un paso
-				//this.paso++;				
+
+				//mas producto? SI -> vuelve a **1 (paso4)
+				if(Oferta.getEstado().compareToIgnoreCase("vendida")==0){
+					
+					this.paso	= 0;
+					this.Estado = "DONE";
+					actor.setEstado("IDLE");
+					break;
+					
+				}else{// if sin respuesta - busca en red las demandas
+					
+					this.paso=4;
+				}
+					
 				break;
 			default:
 				this.Estado ="DONE";
