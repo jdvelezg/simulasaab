1c7a4c18b4c75bc029bc14a634ca0255ad25c2d8
jdvelezg <jdvelezg@users.noreply.github.com>
2014-09-03 16:01:17 -0500

    add getter and Setters

--- src/simulaSAAB/modeloSimulacion/comunicacion/Oferta.java
+++ src/simulaSAAB/modeloSimulacion/comunicacion/Oferta.java
@@ -5,6 +5,7 @@ package simulaSAAB.modeloSimulacion.comunicacion;
 
 import simulaSAAB.modeloSimulacion.AmbienteLocal;
 import simulaSAAB.modeloSimulacion.Producto;
+import simulaSAAB.modeloSimulacion.agentes.AgenteSaab;
 
 /**
  * @author dampher
@@ -12,19 +13,98 @@ import simulaSAAB.modeloSimulacion.Producto;
  */
 public class Oferta extends Demanda {
 	
-	private int ID;
+	private int id;
 	
-	private double CantidadPromedio;
+	private double Cantidad;
 	
-	private Producto producto;
+	private Producto Producto;
+	
+	private Double Precio;
 	
 	private AmbienteLocal Ubicacion;
+	
+	private AgenteSaab Ofertante;
+	
+	private String Estado;//vigente - caducada - vendida
+	
 	
 	/**
 	 * Constructor
 	 */
 	public Oferta(){
+		super();
+	}
+	
+	/**
+	 * Constructor
+	 * @param cantidad
+	 * @param p
+	 * @param a
+	 */
+	public Oferta(double cantidad, Producto p, AgenteSaab a){
 		
 	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public double getCantidad() {
+		return Cantidad;
+	}
+
+	public void setCantidad(double cantidad) {
+		Cantidad = cantidad;
+	}
+
+	public Producto getProducto() {
+		return Producto;
+	}
+
+	public void setProducto(Producto producto) {
+		Producto = producto;
+	}
+
+	public Double getPrecio() {
+		return Precio;
+	}
+	
+	public Double getPrecioUnitario(){
+		return Precio/Cantidad;
+	}
+
+	public void setPrecio(Double precio) {
+		Precio = precio;
+	}
+
+	public AmbienteLocal getUbicacion() {
+		return Ubicacion;
+	}
+
+	public void setUbicacion(AmbienteLocal ubicacion) {
+		Ubicacion = ubicacion;
+	}
+
+	public AgenteSaab getOfertante() {
+		return Ofertante;
+	}
+
+	public void setOfertante(AgenteSaab ofertante) {
+		Ofertante = ofertante;
+	}
+
+	public String getEstado() {
+		return Estado;
+	}
+
+	public void setEstado(String estado) {
+		Estado = estado;
+	}
+	
+	
 
 }
