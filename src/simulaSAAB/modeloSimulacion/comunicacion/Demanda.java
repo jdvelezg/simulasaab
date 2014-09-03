800a93af254c4381fec68cafc311ab5d07684afa
jdvelezg <jdvelezg@users.noreply.github.com>
2014-09-03 16:00:39 -0500

    add getter and setters

--- src/simulaSAAB/modeloSimulacion/comunicacion/Demanda.java
+++ src/simulaSAAB/modeloSimulacion/comunicacion/Demanda.java
@@ -5,6 +5,7 @@ package simulaSAAB.modeloSimulacion.comunicacion;
 
 import simulaSAAB.modeloSimulacion.AmbienteLocal;
 import simulaSAAB.modeloSimulacion.Producto;
+import simulaSAAB.modeloSimulacion.agentes.AgenteSaab;
 
 /**
  * @author dampher
@@ -12,13 +13,19 @@ import simulaSAAB.modeloSimulacion.Producto;
  */
 public class Demanda {
 	
-	private int ID;
+	private int id;
 	
-	private double CantidadPromedio;
+	private double Cantidad;
 	
-	private Producto producto;
+	private Producto Producto;
+	
+	private Double Precio;
 	
 	private AmbienteLocal Ubicacion;
+	
+	private AgenteSaab Demandante;
+	
+	private String Estado;//vigente - caducada - vendida
 	
 	
 	/**
@@ -26,6 +33,82 @@ public class Demanda {
 	 */
 	public Demanda(){
 		
+		this.Estado = "vigente";
+	}
+
+
+	public int getID() {
+		return id;
+	}
+
+
+	public void setID(int id) {
+		this.id = id;
+	}
+
+
+	public double getCantidad() {
+		return Cantidad;
+	}
+
+
+	public void setCantidad(double cantidad) {
+		Cantidad = cantidad;
+	}	
+
+
+	public Double getPrecio() {
+		return Precio;
+	}
+	
+	public Double getPrecioUnitario(){
+		return Precio/Cantidad;
+	}
+	
+
+
+	public void setPrecio(Double precio) {
+		Precio = precio;
+	}
+
+
+	public Producto getProducto() {
+		return Producto;
+	}
+
+
+	public void setProducto(Producto producto) {
+		Producto = producto;
+	}
+
+
+	public AmbienteLocal getUbicacion() {
+		return Ubicacion;
+	}
+
+
+	public void setUbicacion(AmbienteLocal ubicacion) {
+		Ubicacion = ubicacion;
+	}
+
+
+	public AgenteSaab getDemandante() {
+		return Demandante;
+	}
+
+
+	public void setDemandante(AgenteSaab demandante) {
+		Demandante = demandante;
+	}
+
+
+	public String getEstado() {
+		return Estado;
+	}
+
+
+	public void setEstado(String estado) {
+		Estado = estado;
 	}
 	
 
